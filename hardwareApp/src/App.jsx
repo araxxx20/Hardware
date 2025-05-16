f6da7e8535c0734fd4062a71722542b9805824cb
araxxx20 <hernandezara077@gmail.com>
2025-05-16 18:01:23 +0800

    pos

--- hardwareApp/src/App.jsx
+++ hardwareApp/src/App.jsx
@@ -1,27 +1,24 @@
-// ramenApp/src/App.jsx
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Dashboard from './pages/dashboard';
 import Inventory from './pages/inventory';
 import MobileOrder from './pages/mobileOrder';
 import POS from './pages/POS';
-import SalesReport from './pages/salesReport';
+import SalesReport from './pages/salesreport'; // <-- Make sure this exists
 
 function App() {
   return (
-  <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/inventory" element={<Inventory />} />
-          <Route path="/mobile-order" element={<MobileOrder />} />
-          <Route path="/pos" element={<POS />} />
-          <Route path="/salesreport" element={<SalesReport />} />
-        </Routes>
-      </BrowserRouter>
-  </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/mobile-order" element={<MobileOrder />} />
+        <Route path="/pos" element={<POS />} />
+        <Route path="/salesreport" element={<SalesReport />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
